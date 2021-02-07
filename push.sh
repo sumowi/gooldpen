06260505c28319d5280bb07d04ede503c12c625d
sumowi <sumowi@163.com>
2021-02-08 07:03:16 +0800

    gooldpen,yf_Ming

--- push.sh
+++ push.sh
@@ -1,7 +1,12 @@
 sh public.sh
+
 #git init
-#git remote add origin git@e.coding.net:sumowi/www/gooldpen.git
-#git remote set-url origin git@e.coding.net:sumowi/www/gooldpen.git
+#git remote add origin git@github.com:sumowi/gooldpen.git
+#git remote add giteeorigin git@gitee.com:creativeflow/gooldpen.git
+#git remote add codingorigin git@e.coding.net:sumowi/www/gooldpen.git
+
 git add .
 git commit -m "gooldpen,yf_Ming"
 git push origin master -f
+git push giteeorigin master -f
+git push codingorigin master -f
